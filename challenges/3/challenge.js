40446199a5f107bdb25a6fb207b4a614b3fdcb40
Andrei Bastos <andreibastos.developer@gmail.com>
2020-10-24 19:50:05 -0300

    chore: challenge 3

--- challenges/3/challenge.js
+++ challenges/3/challenge.js
@@ -16,7 +16,55 @@
  * Você pode considerar que nas entradas dos testes
  * a > b sempre.
  */
+const isPrime = require('../../utils/isPrime')
 
-const MDC = (a, b) => {}
+const MDC = (a, b) => {
+  // decompoe cada entrada
+  const decomposedA = decomposition(a)
+  const decomposedB = decomposition(b)
+
+  // separa os divisores que são comuns as duas decomposições
+  const commons = Object.keys(decomposedA).filter((divisor) =>
+    Object.keys(decomposedB).includes(divisor),
+  )
+
+  // resultado é a multiplicação dos divisores comuns
+  return commons.reduce((accumulated, common) => {
+    // quantidade em cada decomposição
+    const countA = decomposedA[common]
+    const countB = decomposedB[common]
+
+    // minimo dos dois
+    const min = Math.min(countA, countB)
+
+    // retorna o acumulado vezes a quantidade vezes em comum
+    return accumulated * common ** min
+  }, 1) // inicia com 1, para realizar o acumulado da multiplicação
+}
+
+const decomposition = (n) => {
+  // filtra os numeros primos
+  const primes = [...Array(n).keys(), n].filter(isPrime)
+  let decomposed = {} // saida decomposta
+
+  let dividend = n // primeiro valor a ser dividido
+  let index = 0 // posição do dividor atual
+  let divider = primes[index] // primeiro divisor
+
+  // fica dividindo até chegar a 1 ou passar por todos numeros primos
+  while (dividend > 1 && index < primes.length - 1) {
+    if (dividend % divider == 0) {
+      if (!decomposed[divider]) {
+        decomposed[divider] = 0
+      }
+      decomposed[divider]++
+      dividend = dividend / divider // atualiza o dividendo
+    } else {
+      index++
+      divider = primes[index] // caso não seja múltiplo, pega o proximo dividor
+    }
+  }
+  return decomposed
+}
 
 module.exports = MDC
